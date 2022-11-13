8a4bc373c32bef258fdd48b0f202d827ce1bf8d5
Alex Mejias <alexdmejias@gmail.com>
2022-11-13 12:56:54 -0500

    first pass, not fully working

--- index.js
+++ index.js
@@ -76,65 +76,66 @@ async function getPort(portArg) {
   return [port, parser];
 }
 
-const initialCommands = [];
-const finalCommands = [];
+// class FileLinesQueue {
+//   constructor(filePath, verbose) {
+//     this.filePath = filePath;
+//     this.chunkSize = 5;
+//     this.lastLineRequested = 0;
+//     this.initAmountOfLines = 10;
+//     this.minAmountOfLinesInBuffer = 5;
+//     this.buffer = [];
+//     this.isDoneRequesting = false;
+//   }
 
-class FileLinesQueue {
-  constructor(filePath, verbose) {
-    this.filePath = filePath;
-    this.chunkSize = 5;
-    this.lastLineRequested = 0;
-    this.initAmountOfLines = 10;
-    this.minAmountOfLinesInBuffer = 5;
-    this.buffer = [];
-    this.isDoneRequesting = false;
-  }
+//   async fillBuffer() {
+//     if (this.isDoneRequesting) {
+//       return;
+//     }
 
-  async fillBuffer() {
-    if (this.isDoneRequesting) {
-      return;
-    }
+//     const lines = (await fs.readFile(this.filePath, 'utf-8')).split('\n');
+//     const newLines = lines.slice(this.lastLineRequested, this.lastLineRequested + this.chunkSize);
 
-    const lines = (await fs.readFile(this.filePath, 'utf-8')).split('\n');
-    const newLines = lines.slice(this.lastLineRequested, this.lastLineRequested + this.chunkSize);
+//     if (
+//       newLines.length < this.minAmountOfLinesInBuffer &&
+//       this.lastLineRequested !== 0 &&
+//       !this.isDoneRequesting
+//     ) {
+//       this.isDoneRequesting = true;
+//     }
+//     this.lastLineRequested += this.chunkSize;
+//     this.buffer.push(...newLines);
 
-    if (
-      newLines.length < this.minAmountOfLinesInBuffer &&
-      this.lastLineRequested !== 0 &&
-      !this.isDoneRequesting
-    ) {
-      this.isDoneRequesting = true;
-    }
-    this.lastLineRequested += this.chunkSize;
-    this.buffer.push(...newLines);
+//     return newLines;
+//   }
 
-    return newLines;
-  }
+//   async getNewLinesIfNeeded() {
+//     if (this.buffer.length < this.minAmountOfLinesInBuffer) {
+//       await this.fillBuffer();
+//     } else {
+//       return;
+//     }
+//   }
 
-  async getNewLinesIfNeeded() {
-    if (this.buffer.length < this.minAmountOfLinesInBuffer) {
-      await this.fillBuffer();
-    } else {
-      return;
-    }
-  }
+//   async getNextLine() {
+//     await this.getNewLinesIfNeeded();
 
-  async getNextLine() {
-    await this.getNewLinesIfNeeded();
+//     return this.buffer.shift();
+//   }
 
-    return this.buffer.shift();
-  }
-
-  hasMoreLines() {
-    return !!this.buffer.length;
-  }
-}
+//   hasMoreLines() {
+//     return !!this.buffer.length;
+//   }
+// }
 
 class Machine {
-  constructor(port, verbose) {
+  constructor({ port, verbose, initCommands, endCommands, file }) {
     this.port = port;
+    this.file = file;
     this.verbose = verbose;
+    this.initCommands = initCommands;
+    this.endCommands = endCommands;
 
+    this.buffer = [];
     this.isOk = false;
     this.isReady = false;
     this.isDone = false;
@@ -148,10 +149,26 @@ class Machine {
     this.machineState = {};
   }
 
+  *getCommand() {
+    yield* this.initCommands;
+    yield* this.buffer;
+    yield* this.endCommands;
+  }
+
   sendCommand(command) {
     if (this.verbose) console.log(chalk.red('MACHINE::TX::', 'sendCommand', command));
     this.port.write(`${command}\n`);
     this.isSendingCommand = true;
+  }
+
+  resetState() {}
+
+  async fillBuffer() {
+    this.buffer = (await fs.readFile(this.file, 'utf-8')).split('\n');
+  }
+
+  removeLineFromBuffer() {
+    this.buffer.shift();
   }
 
   parseMessage(line, lineNumber) {
@@ -160,21 +177,16 @@ class Machine {
     if (line === 'ok') {
       this.isOk = true;
       this.isSendingCommand = false;
-      // this.readyForNextCommand = true;
-      // } else if (line === `[MSG:'$H'|'$X' to unlock]`) {
-      // this.machineIsLocked = true;
-      // } else if (line === '[MSG:Caution: Unlocked]') {
-      // this.machineIsLocked = false;
-      // } else if (line.includes('Error'))
     } else if (line === '[MSG:Pgm End]') {
       this.isDone = true;
     } else if (line[0] === '$' && line[1] !== 'N') {
       const a = line.indexOf('=');
       const code = line.slice(1, a);
-      // console.log('!!!!!!!!', code);
       if (this.verbose)
         stdout.write(chalk.green(`GRBL SETTING: $${code} = ${GRBL_SETTINGS[code]}\n`));
-    } else if (line[0] === '<') {
+    } else if (!this.isReady && line === `Grbl 1.1h ['$' for help]`) {
+      this.isReady = true;
+    } else if (line[0] === '<' && line.slice(-1) === '>') {
       const state = parseStatusMessage(line);
       this.machineState = state;
       console.log('@@@@@@@@', state);
@@ -193,6 +205,12 @@ class Machine {
   }
 }
 
+// TODO this needs _something_ to better detect machine state
+function getShouldProceed(line) {
+  // console.log('!!!!!!!!', 'getShouldProceed', line);
+  return ['ok', `[MSG:'$H'|'$X' to unlock]`, `Grbl 1.1h ['$' for help]`].includes(line);
+}
+
 function getJobDuration(startTime) {
   const now = new Date();
   const seconds = now - startTime;
@@ -207,66 +225,49 @@ async function sendFileCallback({ filePath, port, verbose }) {
 
   const [sp, parser] = await getPort(port);
 
-  const q = new FileLinesQueue(filePath, verbose);
-  const m = new Machine(sp, verbose);
+  // const q = new FileLinesQueue(filePath, verbose);
+  const m = new Machine({
+    file: filePath,
+    port: sp,
+    verbose,
+    initCommands: ['??', RUN_HOMING_CYCLE],
+    endCommands: ['G91 X10']
+  });
 
-  await q.fillBuffer();
-  let jobStartTime;
+  await m.fillBuffer();
+  // let jobStartTime;
 
   let lineCounter = 0;
+
+  let commandsBuffer = m.getCommand();
+  // let prevSentLine;
+  // let prevReceievedLine;
 
   parser.on('data', async (line) => {
     if (verbose) console.log(chalk.green('RX::', line));
 
     m.parseMessage(line, lineCounter);
 
-    if (!m.isReady && line === `Grbl 1.1h ['$' for help]`) {
-      m.isReady = true;
+    const shouldProceed = getShouldProceed(line);
 
+    if (shouldProceed === false) {
+      console.log('!!!!!!!!', 'getShouldProceed() said to stop because: ', line);
       return;
     }
 
-    if (initialCommands.length) {
-      const cmd = initialCommands.shift();
-      m.sendCommand(cmd);
-    } else if (!m.isDone) {
-      if (!initialCommands.length /* && isOk */ && !m.isHomed && !m.isHoming) {
-        if (!jobStartTime) {
-          jobStartTime = new Date();
-        }
-        m.sendCommand(RUN_HOMING_CYCLE);
-        m.isHoming = true;
-      }
+    const nextGcodeLine = commandsBuffer.next();
 
-      if (m.isHoming && !m.isHomed && m.isOk) {
-        m.isHoming = false;
-        m.isHomed = true;
-        // m.isReadyForFile = true;
-
-        // m.sendCommand(VIEW_SETTINGS);
-      }
-
-      if (m.isHomed && m.isOk && q.hasMoreLines() && !m.isSendingCommand) {
-        const line = await q.getNextLine();
-        lineCounter++;
-        m.sendCommand(line, lineCounter);
-      }
-
-      if (!q.hasMoreLines()) {
-        m.isDone = true;
-      }
-
-      // TODO need something like this
-      // if ((!q.hasMoreLines() && m.isOk) || m.isDone) {
-      //   const jobDuration = getJobDuration(jobStartTime);
-      //   if (verbose) console.log(`successfully finished job in ${jobDuration}`);
-      //   exit(0);
-      // }
-    } else if (finalCommands.length) {
-      console.log('!!!!!!!!', 'final block', finalCommands);
-      const cmd = finalCommands.shift();
-      m.sendCommand(cmd);
+    if (nextGcodeLine.done === true) {
+      console.log('!!!!!!!!', 'ran out of gcode');
+      exit(0);
+    } else {
+      m.sendCommand(nextGcodeLine.value);
+      // TODO check if we are removing a line from the gcode buffer, if so remove the top line from the bufferto reduce memory
+      // m.removeLineFromBuffer();
     }
+
+    // prevSentLine = nextGcodeLine.value;
+    // prevReceievedLine = line;
   });
 }
 
